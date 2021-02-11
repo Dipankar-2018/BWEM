7f4620ecfd09cb3d7cbe32d5e4ba96ad5b31b242
Dipankar-2018 <dipankarrabha2011@gmail.com>
2021-02-11 18:59:19 +0530

    model

--- admin/index.php
+++ admin/index.php
@@ -54,7 +54,7 @@ function getDistCount($cat,$district){
               <div class="icon">
               <i class="fas fa-chart-line"></i>
               </div>
-              <a href="data.php" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a onclick="" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -69,7 +69,7 @@ function getDistCount($cat,$district){
               <div class="icon">
               <i class="fas fa-chart-area"></i>
               </div>
-              <a href="data.php" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a onclick="" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -84,7 +84,7 @@ function getDistCount($cat,$district){
               <div class="icon">
               <i class="fas fa-chart-bar"></i>
               </div>
-              <a href="data.php" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a onclick="" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -100,7 +100,7 @@ function getDistCount($cat,$district){
               <i class="fas fa-chart-pie"></i>
              
               </div>
-              <a href="data.php" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a onclick="" class="small-box-footer" data-toggle="modal" data-target="#model-desh">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -169,73 +169,68 @@ function getDistCount($cat,$district){
                 <table class="table table-bordered">
                   <thead>
                     <tr>
-                      <th style="width: 10px">#</th>
+                      
                       <th>Category</th>
-                      <th>Progress</th>
+                      <th>Accepted</th>
+                      <th>Rejected</th>
+                      <th>Pending</th>
                       <th style="width: 40px">Total</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>1.</td>
+                    <tr>                    
                       <td>Self Help Group</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">1000</span></td>
-                    </tr>
-                    <tr>
-                      <td>2.</td>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr>     
+                    
+                    <tr>                    
                       <td>Entreprenures</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
-                    </tr>
-                    <tr>
-                      <td>3.</td>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr> 
+
+                    <tr>                    
                       <td>NGO'S</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
-                    </tr>
-                    <tr>
-                      <td>4.</td>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr> 
+
+                    <tr>                    
                       <td>Association</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                    </tr>
-                    <tr>
-                      <td>5.</td>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr> 
+
+
+                    <tr>                    
                       <td>Trainer</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                    </tr>
-                    <tr>
-                      <td>6.</td>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr> 
+
+
+                    <tr>                    
                       <td>Trainee</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                    </tr>
+                      <td>500</td>
+                      <td>20</td>
+                      <td>30</td>
+                      <td>45</td>
+                    </tr> 
+
+
+                    
+                                    
                   </tbody>
                 </table>
               </div>
